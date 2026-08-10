1694426af444a67dde4c5d5968b65e744877994a
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-08-10 06:12:38 +0200

    public: add RevertServerConVars to ICvar

--- public/icvar.h
+++ public/icvar.h
@@ -178,6 +178,10 @@ protected:
 
 	virtual ICVarIteratorInternal	*FactoryInternalIterator( void ) = 0;
 	friend class Iterator;
+
+public:
+	// GMod
+	virtual void RevertServerConVars( bool ) = 0;
 };
 
 inline ICvar::Iterator::Iterator(ICvar *icvar)
